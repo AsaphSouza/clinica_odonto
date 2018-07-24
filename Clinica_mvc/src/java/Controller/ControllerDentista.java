46a4d42942d245d8e310800e069fd00637c27f68
AsaphSouza <asaphsouza1605@gmail.com>
2018-07-24 12:19:56 -0300

    b

--- Clinica_mvc/src/java/Controller/ControllerDentista.java
+++ Clinica_mvc/src/java/Controller/ControllerDentista.java
@@ -10,66 +10,76 @@ import model.entidades.Dentista;
 @ManagedBean
 @SessionScoped
 public class ControllerDentista {
-	private DentistaModel dentistaModel;
-	private Dentista dentista;
 
-	public ControllerDentista() {
-		dentistaModel = new DentistaModel();
-		dentista = new Dentista();
-	}
+    private DentistaModel dentistaModel;
+    private Dentista dentista;
+    private String cro;
 
-	public void cadastrarDentista() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		try {
-			dentistaModel.inserirDentista(dentista);
-			// context.addMessage(null, new FacesMessage("Cadastro Efetuado!"));
-		} catch (Exception ex) {
-			context.addMessage(null, new FacesMessage(ex.getMessage()));
-		}
-	}
+    public ControllerDentista() {
+        dentistaModel = new DentistaModel();
+        dentista = new Dentista();
+    }
 
-	public void deletarDentista() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		try {
-			dentistaModel.deletarDentista(dentista);
-		} catch (Exception ex) {
-			context.addMessage(null, new FacesMessage(ex.getMessage()));
-		}
-	}
+    public void cadastrarDentista() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            dentistaModel.inserirDentista(dentista);
+            // context.addMessage(null, new FacesMessage("Cadastro Efetuado!"));
+        } catch (Exception ex) {
+            context.addMessage(null, new FacesMessage(ex.getMessage()));
+        }
+    }
 
-	public void atualizarDentista() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		try {
-			dentistaModel.atualizarDentista(dentista);
-		} catch (Exception ex) {
-			context.addMessage(null, new FacesMessage(ex.getMessage()));
-		}
-	}
+    public void deletarDentista() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            dentistaModel.deletarDentista(dentista);
+        } catch (Exception ex) {
+            context.addMessage(null, new FacesMessage(ex.getMessage()));
+        }
+    }
 
-	public Dentista buscarPeloCRO() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		try {
-			return dentistaModel.buscarPorCRO(dentista.getCro());
-		} catch (Exception ex) {
-			context.addMessage(null, new FacesMessage(ex.getMessage()));
-			return null;
-		}
-	}
+    public void atualizarDentista() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            dentistaModel.atualizarDentista(dentista);
+        } catch (Exception ex) {
+            context.addMessage(null, new FacesMessage(ex.getMessage()));
+        }
+    }
 
-	public DentistaModel getDentistaModel() {
-		return dentistaModel;
-	}
+    public Dentista buscarPeloCRO() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            return dentistaModel.buscarPorCRO(cro);
+        } catch (Exception ex) {
+            context.addMessage(null, new FacesMessage(ex.getMessage()));
+            return null;
+        }
+    }
 
-	public void setDentistaModel(DentistaModel dentistaModel) {
-		this.dentistaModel = dentistaModel;
-	}
+    public String getCro() {
+        return cro;
+    }
 
-	public Dentista getDentista() {
-		return dentista;
-	}
+    public void setCro(String cro) {
+        this.cro = cro;
+    }
 
-	public void setDentista(Dentista dentista) {
-		this.dentista = dentista;
-	}
+    public DentistaModel getDentistaModel() {
+        return dentistaModel;
+    }
+
+    public void setDentistaModel(DentistaModel dentistaModel) {
+        this.dentistaModel = dentistaModel;
+    }
+
+    public Dentista getDentista() {
+        return dentista;
+    }
+
+    public void setDentista(Dentista dentista) {
+        this.dentista = dentista;
+    }
 
 }
